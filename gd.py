5cd28bf0302b011033c0d1fa5b507af54574152c
Minsang Kim <kmswin1@korea.ac.kr>
2019-06-11 13:42:29 +0900

    change operation bug (from + to - , in f(x) + f'(x)*dx)

--- gd.py
+++ gd.py
@@ -8,7 +8,7 @@ def min_gd(fun, x0, grad, args=()):
 
     while (np.max(np.abs(dx)) > 1e-07):
         t = 1
-        while fun(x0+t*dx,*args) > fun(x0,*args) + alpha*t*dx.T/len(dx)@dx/len(dx):
+        while fun(x0+t*dx,*args) > fun(x0,*args) - alpha*t*dx.T@dx:
             t *= beta
         x0 = x0 + t*dx
         dx = -grad(x0,*args)
